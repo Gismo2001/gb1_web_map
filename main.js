ffb76f009a484bba4426962569bcd59a22eeb7da
Gismo2001 <rankenpunk@googlemail.com>
2024-02-26 22:29:36 +0100

    Versuch mit Kompass

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-import { test } from './meineDatei.js';
-
-const compassStyle = new ol.style.Style({
-  fill: new ol.style.Fill({
-    color: 'rgba(0, 0, 255, 0.2)',
-  }),
-  image: new ol.style.Icon({
-    src: './data/location-heading.svg',
-    imgSize: [27, 55],
-    rotateWithView: true,
-  }),
-});
-//ol.layer.setStyle(compassStyle);
+import { getStyleForArtEin, getStyleForArtFSK, son_linStyle, son_punStyle, queStyle, dueStyle, wehStyle, bru_nlwknStyle, bru_andereStyle, sleStyle, km10scalStyle } from './mystyles.js';
+import {Fill, Icon, Style} from 'ol/style';
 
 
-// Definieren Sie die Vektorquelle
-const source = new ol.source.Vector();
+
 
 // Funktion zur Adresssuche
 window.searchAddress = function searchAddress() {
@@ -43,189 +31,6 @@ window.searchAddress = function searchAddress() {
       });
 }
 
-const son_linStyle = new ol.style.Style({
-  stroke: new ol.style.Stroke({
-    color: 'rgba(209, 32, 253, 1)',
-    width: 2,
-  }),
-});
-
-const son_punStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1)' }),
-    stroke: new ol.style.Stroke({
-      color: 'black',
-      width: 2
-    }),
-    points: 4,
-    radius: 7,
-    angle: Math.PI / 4
-  })
-});
-
-const einStyle = new ol.style.Style({
-  image: new ol.style.Circle({
-    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1)'}), 
-    stroke: new ol.style.Stroke({
-      color: 'black',
-      width: 0.5
-    }),
-    radius: 7
-  })
-});
-
-const queStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1'}),
-    stroke: new ol.style.Stroke({
-      color: 'black',
-      width: .5
-    }),
-    points: 4,
-    radius: 7,
-    angle: Math.PI / 2
-  })
-});
-
-const dueStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1'}),
-    stroke: new ol.style.Stroke({
-      color: 'black',
-      width: 2
-    }),
-    points: 4,
-    radius: 7,
-    angle: Math.PI / 4
-  })
-});
-
-const wehStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color: 'green'}),
-    stroke: new ol.style.Stroke({
-      color: 'black',
-      width: 2
-    }),
-    points: 3,
-    radius: 7,
-    rotation: 0  // Setzen Sie die Rotation auf 0 für ein Dreieck
-  })
-});
-
-const bru_nlwknStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color: 'blue'}),
-    stroke: new ol.style.Stroke({color: 'grey', width: 1}),
-    points: 4,
-    radius: 7,
-    angle: Math.PI / 4
-  })
-});
-
-const bru_andereStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color:'rgba(100, 100, 100, 1)'}),
-    stroke: new ol.style.Stroke({color: 'grey',width: 1}),
-    points: 4,
-    radius: 6,
-    angle: Math.PI / 4
-  })
-});
-
-const sleStyle = new ol.style.Style({
-  image: new ol.style.RegularShape({
-    fill: new ol.style.Fill({color: 'red'}),
-    stroke: new ol.style.Stroke({
-      color: 'grey',
-      width: 2
-    }),
-    points: 4,
-    radius: 7,
-    angle: Math.PI / 4
-  })
-});
-
-const km10scalStyle = new ol.style.Style({
-  stroke: new ol.style.Stroke({
-    color: 'grey',
-    width: .5
-  })
-});
-
-function getStyleForArtEin(feature) {
-  const artValue = feature.get('Ein_ord');
-  let fillColor, strokeColor;
-
-  switch (artValue) {
-    case '1. Ordnung':
-      fillColor = 'rgba(0, 68, 255, .8)';
-      strokeColor = 'black';
-      break;
-    case '2. Ordnung':
-      fillColor = 'rgba(214, 0, 0, .8)';
-      strokeColor = 'black';
-      break;
-    case '3. Ordnung':
-      fillColor = 'rgba(114, 114, 114, .8)';
-      strokeColor = 'black';
-      break;
-    case 'Sonstige':
-      fillColor = 'rgba(27, 117, 0, .8)';
-      strokeColor = 'black';
-      break;
-    default:
-      fillColor = 'grey';
-      strokeColor = 'grey';
-  }
-
-  return new ol.style.Style({
-    image: new ol.style.Circle({
-      fill: new ol.style.Fill({
-        color: fillColor
-      }),
-      stroke: new ol.style.Stroke({
-        color: strokeColor,
-        width: 0.5
-      }),
-      radius: 7
-    })
-  });
-}
-
-//Berechnung Style für FSK
- function getStyleForArtFSK(feature) {
-  const artValue = feature.get('Art');
-  let fillColor, strokeColor;
-
-  switch (artValue) {
-    case 'p':
-      fillColor = 'rgba(200, 200, 200, .7)';
-      strokeColor = 'grey';
-      break;
-    case 'o':
-      fillColor = 'rgba(255, 220, 220, .7)';
-      strokeColor = 'grey';
-      break;
-    case 'l':
-      fillColor = 'rgba(255, 190, 150, .7)';
-      strokeColor = 'black';
-      break;
-    default:
-      fillColor = 'rgba(255, 255, 255, 1)';
-      strokeColor = 'grey';
-  }
-
-  return new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: fillColor
-    }),
-    stroke: new ol.style.Stroke({
-      color: strokeColor,
-      width: 0.5
-    })
-  });
-}
 
 var attribution = new ol.control.Attribution({
   collapsible: false
@@ -243,10 +48,6 @@ var map = new ol.Map({
   controls: ol.control.defaults().extend([attribution])
 });
 //////////////
-
-const mapProjection = map.getView().getProjection();
-console.log(mapProjection.getCode());
-
 
 //FSK Layer
 var exp_allgm_fsk_layer = new ol.layer.Vector({
@@ -282,7 +83,7 @@ var exp_bw_ein_layer = new ol.layer.Vector({
   format: new ol.format.GeoJSON(),
   url: function (extent) {return './myLayers/exp_bw_ein.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
   title: 'ein', // Titel für den Layer-Switcher
-  style: test,
+  style: getStyleForArtEin,
   visible: false
 });
 
@@ -746,6 +547,7 @@ document.getElementById('popup-closer').onclick = function () {
   return false;
 };
 
+const source = new ol.source.Vector();
 
 navigator.geolocation.watchPosition(
   function (pos) {
@@ -756,14 +558,12 @@ navigator.geolocation.watchPosition(
     const circle = new ol.geom.Circle(ol.proj.fromLonLat(coords), accuracy);
     const circularPolygon = ol.geom.Polygon.fromCircle(circle, 64); // 64 segments for a smoother circle
 
-    source.clear(true);
+    source.clear(true); // 'source' statt 'ol.source'
     source.addFeatures([
       new ol.Feature({
         geometry: circularPolygon.transform('EPSG:4326', map.getView().getProjection()),
       }),
       new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat(coords))),
-
-      
     ]);
   },
   function (error) {
@@ -773,6 +573,7 @@ navigator.geolocation.watchPosition(
     enableHighAccuracy: true,
   }
 );
+
 
 const locate = document.createElement('div');
 locate.className = 'ol-control ol-unselectable locate';
